1a5166aadd692dfd3c96bdbf132baf724a4999a6
Kevin Huck <khuck@cs.uoregon.edu>
2019-09-09 12:56:03 -0700

    Fixing static build

--- perfstubs_api/Timer.cpp
+++ perfstubs_api/Timer.cpp
@@ -85,27 +85,38 @@ static void InitializeLibrary(void)
 }
 
 #ifdef PERFSTUBS_USE_STATIC
+
+#if defined(__clang__) && defined(__APPLE__)
+#define PS_WEAK_PRE
+#define PS_WEAK_POST __attribute__((weak_import))
+#define PS_WEAK_POST_NULL __attribute__((weak_import))
+#else
+#define PS_WEAK_PRE __attribute__((weak))
+#define PS_WEAK_POST
+#define PS_WEAK_POST_NULL
+#endif
+
 extern "C"
 {
-    void perftool_init(void) __attribute((weak));
-    void perftool_register_thread(void) __attribute((weak));
-    void perftool_exit(void) __attribute((weak));
-    void perftool_dump_data(void) __attribute((weak));
-    void* perftool_timer_create(const char *) __attribute((weak));
-    void perftool_timer_start(const void *) __attribute((weak));
-    void perftool_timer_stop(const void *) __attribute((weak));
-    void perftool_dynamic_phase_start(const char *, int) __attribute((weak));
-    void perftool_dynamic_phase_stop(const char *, int) __attribute((weak));
-    void* perftool_create_counter(const char *) __attribute((weak));
-    void perftool_sample_counter(const void *, double) __attribute((weak));
-    void perftool_metadata(const char *, const char *) __attribute((weak));
-    void perftool_get_timer_data(perftool_timer_data_t *) __attribute((weak));
-    void perftool_get_counter_data(perftool_counter_data_t *)
-        __attribute((weak));
-    void perftool_get_metadata(perftool_metadata_t *) __attribute((weak));
-    void perftool_free_timer_data(perftool_timer_data_t *) __attribute((weak));
-    void perftool_free_counter_data(perftool_counter_data_t *) __attribute((weak));
-    void perftool_free_metadata(perftool_metadata_t *) __attribute((weak));
+    PS_WEAK_PRE void perftool_init(void) PS_WEAK_POST;
+    PS_WEAK_PRE void perftool_register_thread(void) PS_WEAK_POST;
+    PS_WEAK_PRE void perftool_exit(void) PS_WEAK_POST;
+    PS_WEAK_PRE void perftool_dump_data(void) PS_WEAK_POST;
+    PS_WEAK_PRE void* perftool_timer_create(const char *) PS_WEAK_POST;
+    PS_WEAK_PRE void perftool_timer_start(const void *) PS_WEAK_POST;
+    PS_WEAK_PRE void perftool_timer_stop(const void *) PS_WEAK_POST;
+    PS_WEAK_PRE void perftool_dynamic_phase_start(const char *, int) PS_WEAK_POST;
+    PS_WEAK_PRE void perftool_dynamic_phase_stop(const char *, int) PS_WEAK_POST;
+    PS_WEAK_PRE void* perftool_create_counter(const char *) PS_WEAK_POST;
+    PS_WEAK_PRE void perftool_sample_counter(const void *, double) PS_WEAK_POST;
+    PS_WEAK_PRE void perftool_metadata(const char *, const char *) PS_WEAK_POST;
+    PS_WEAK_PRE void perftool_get_timer_data(perftool_timer_data_t *) PS_WEAK_POST;
+    PS_WEAK_PRE void perftool_get_counter_data(perftool_counter_data_t *)
+        PS_WEAK_POST;
+    PS_WEAK_PRE void perftool_get_metadata(perftool_metadata_t *) PS_WEAK_POST;
+    PS_WEAK_PRE void perftool_free_timer_data(perftool_timer_data_t *) PS_WEAK_POST;
+    PS_WEAK_PRE void perftool_free_counter_data(perftool_counter_data_t *) PS_WEAK_POST;
+    PS_WEAK_PRE void perftool_free_metadata(perftool_metadata_t *) PS_WEAK_POST;
 }
 #endif
 
