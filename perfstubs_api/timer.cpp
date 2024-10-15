95bb8c2204520d600b2f16c9e0997c802908b193
Kevin Huck <khuck@cs.uoregon.edu>
2024-10-15 08:28:53 -0700

    Adding comment to timer.cpp

--- perfstubs_api/timer.cpp
+++ perfstubs_api/timer.cpp
@@ -1,1 +1,7 @@
+/* this file exists so that the pyproject.toml build doesn't have
+   to build a mix of C and C++ files - because the -std=C++17 flag
+   gets passed to both. Regardless, the ABI that is exposed to
+   Python is extern "C" in python.cpp, and the timer.c code can
+   have mangled symbols because they only get called by python.cpp
+ */
 #include "timer.c"
